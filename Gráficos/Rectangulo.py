8706b9931273538b7789159faa58bc0667298d91
LiliConalep2025 <prof.liliana.mondragon@qro.conalep.edu.mx>
2025-07-02 20:55:12 -0600

    Rectangulo
    Muestra un rectangulo

---
+++ Gráficos/Rectangulo.py
@@ -0,0 +1,27 @@
+import turtle
+import time
+
+
+#Rectangulo
+#Lado inferior
+turtle.forward(100)
+time.sleep(2)
+turtle.left(90)
+time.sleep(2)
+#Lado derecho
+turtle.forward(50)
+time.sleep(2)
+turtle.left(90)
+time.sleep(2)
+#Lado superior
+turtle.forward(100)
+time.sleep(2)
+turtle.left(90)
+time.sleep(2)
+#Lado izquierdo
+turtle.forward(50)
+time.sleep(2)
+turtle.left(90)
+time.sleep(2)
+
+done()
