c314d402a91784a87fff9f70808f453a1b0635e3
LiliConalep2025 <prof.liliana.mondragon@qro.conalep.edu.mx>
2025-07-02 21:46:08 -0600

    Hexagono
    Hexagono con codigo Turtle

---
+++ Gráficos/Hexagono.py
@@ -0,0 +1,35 @@
+import turtle
+import time
+
+#Lado 1
+turtle.forward(70)
+time.sleep(2)
+turtle.left(60)
+time.sleep(2)
+#Lado 2
+turtle.forward(70)
+time.sleep(2)
+turtle.left(60)
+time.sleep(2)
+#Lado 3
+turtle.forward(70)
+time.sleep(2)
+turtle.left(60)
+time.sleep(2)
+#Lado 4
+turtle.forward(70)
+time.sleep(2)
+turtle.left(60)
+time.sleep(2)
+#Lado 5
+turtle.forward(70)
+time.sleep(2)
+turtle.left(60)
+time.sleep(2)
+#Lado 6
+turtle.forward(70)
+time.sleep(2)
+turtle.left(60)
+time.sleep(2)
+
+turtle.done()
