0fce994696947fee719e07f1f6e91b4770fd7684
LiliConalep2025 <prof.liliana.mondragon@qro.conalep.edu.mx>
2025-07-02 21:33:57 -0600

    Casita
    Casita formada por figuras geometricas

---
+++ Gráficos/Casita.py
@@ -0,0 +1,43 @@
+import turtle
+import time
+#Paredes
+turtle.forward(100)
+time.sleep(2)
+turtle.left(90)
+time.sleep(2)
+turtle.forward(100)
+time.sleep(2)
+turtle.left(90)
+time.sleep(2)
+turtle.forward(100)
+time.sleep(2)
+turtle.left(90)
+time.sleep(2)
+turtle.forward(100)
+time.sleep(2)
+turtle.left(90)
+time.sleep(2)
+#Diagonal 1
+turtle.left(45)
+time.sleep(2)
+turtle.forward(140)
+time.sleep(2)
+#Techo
+turtle.left(90)
+time.sleep(2)
+turtle.forward(70.7)
+time.sleep(2)
+turtle.left(90)
+time.sleep(2)
+turtle.forward(70.7)
+time.sleep(2)
+#Diagonal 2
+turtle.left(90)
+time.sleep(2)
+turtle.forward(140)
+time.sleep(2)
+
+turtle.done()
+
+
+
